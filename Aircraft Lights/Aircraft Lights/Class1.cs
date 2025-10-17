a908ca7ad8e268187ed5aa8d5efff1ca4d1ae471
joelmoxon <moxonjoeljames@gmail.com>
2025-10-17 17:38:49 +0100

    Add on, off and fault methods for light class

---
+++ Aircraft Lights/Aircraft Lights/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aircraft_Light_System
+{
+    public abstract class Light
+    {
+        private string lightId;
+        private bool isOn = false;
+        private bool isFault = false;
+        private string colour;
+
+        protected Light(string id)
+        {
+            lightId = id;
+        }
+
+        // Turn on light if no fault, update GUI and log event
+        public virtual bool TurnOn()
+        {
+            if (!isFault)
+            {
+                isOn = true;
+                GUI.UpdateLightStatus(lightId, isOn);
+                LogFile.WriteEvent(FlightInfo.CurrentTime, lightId, "turned ON"); 
+                return true;
+            }
+            return false;
+        }
+
+        // Turn off light, update GUI and log event
+        public virtual bool TurnOff()
+        {
+            isOn = false;
+            GUI.UpdateLightStatus(lightId, isOn);
+            LogFile.WriteEvent(FlightInfo.CurrentTime, lightId, "turned OFF");
+            return true;
+        }
+
+        // Sets fault status and turns off light if fault is true, and log event
+        public void HasFault(bool faultStatus)
+        {
+            isFault = faultStatus;
+            if (isFault && isOn)
+            {
+                TurnOff();
+            }
+            LogFile.WriteEvent(FlightInfo.CurrentTime, lightId, "FAULT detected ");
+        }
+    }
+}

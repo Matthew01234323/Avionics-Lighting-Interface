8f5c476166375225cb143c371c5e59b55c1b48e5
joelmoxon <moxonjoeljames@gmail.com>
2025-10-20 21:36:28 +0100

    Add DimmingLight class with brightness control. Add AisleLight class with auto brightness adjustment for sunrise/sunset times.

---
+++ Aircraft Lights/Aircraft Lights/AsigleLight.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aircraft_Lights
+{
+    public class AisleLight : DimmingLight
+    {
+        private bool autoBrightnessEnabled = true;
+        public AisleLight(string id) : base(id)
+        {
+
+        }
+        public bool AutoBrightnessEnabled
+        {
+            get { return autoBrightnessEnabled; }
+            set
+            {
+                autoBrightnessEnabled = value;
+                LogFile.WriteEvent(FlightInfo.CurrentTime, LightId, $"Auto-brightness set to {autoBrightnessEnabled}");
+            }
+        }
+
+        // Adjust brightness based on sunrise and sunset times if auto-brightness is enabled
+        public void AdjustForTimeOfDay()
+        {
+            if (autoBrightnessEnabled && IsOn)
+            {
+                TimeSpan currentTime = TimeInfo.CurrentTime;
+                TimeSpan daytimeStart = TimeInfo.SunriseTime;
+                TimeSpan daytimeEnd = TimeInfo.SunsetTime;
+
+                if (currentTime >= daytimeStart && currentTime < daytimeEnd)
+                {
+                    Brightness = 5;
+                }
+                else
+                {
+                    Brightness = 2;
+                }
+
+                LogFile.WriteEvent(FlightInfo.CurrentTime, LightId, $"Adjusting brightness for time of day to {Brightness}");
+
+            }
+        }
+    }
+}
+    
+
---
+++ Aircraft Lights/Aircraft Lights/DimmingLight.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aircraft_Lights
+{
+    public class DimmingLight : InteriorLight
+    {
+        // Brightness level from 1 (dim) to 10 (bright), default is 5
+        private int brightness = 5;
+
+        protected DimmingLight(string id) : base(id)
+        {
+
+        }
+        public int Brightness
+        {
+            get { return brightness; }
+
+            // Set brightness within valid range and log event
+            set
+            {
+                if (value >= 1 && value <= 10)
+                {
+                    brightness = value;
+                    LogFile.WriteEvent(FlightInfo.CurrentTime, LightId, $"brightness set to {brightness}");
+                }
+            }
+
+        }
+    }
+}
+
+

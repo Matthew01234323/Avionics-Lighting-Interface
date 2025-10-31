e1b5f7cf395cca92cca209961685c22d44178ecc
Holt570 <30250524@blackpool.ac.uk>
2025-10-31 15:51:45 +0000

    Created WriteEvent method

--- LogFile.cs
+++ LogFile.cs
@@ -4,9 +4,15 @@ namespace AircraftLightsGUI
 {
     public class LogFile
     {
-        private string filepath = "";
+        private string file_path = "";
+        private string file_name = "";
 
         void OpenFile()
+        {
+            
+        }
+        
+        void WriteEvent(DateTime time, string id, string event_type)
         {
             
         }
--- obj/Debug/net9.0-windows/Avionics-Lighting-Interface.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/Avionics-Lighting-Interface.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Avionics-Lighting-Interface")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1baa0584bb1d2b0efa7e1621e0a2495341531292")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48fcbf3ab9045fa97f2a8ebc5f03408f1bb8b7e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Avionics-Lighting-Interface")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Avionics-Lighting-Interface")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

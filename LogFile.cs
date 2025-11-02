c49f59b7c328440d042d2fc2224157a5fd0865d7
Holt570 <30250524@blackpool.ac.uk>
2025-11-02 08:10:27 +0000

    Implemented SetFileName

--- LogFile.cs
+++ LogFile.cs
@@ -4,12 +4,12 @@ namespace AircraftLightsGUI
 {
     public class LogFile
     {
-        private string file_path = "";
+        private string file_path = "C:\\Users\\samho\\OneDrive\\Documents\\B&FC\\Year_2\\Object_Oriented_Programming\\Assignment_2\\Local_Repo\\Avionics-Lighting-Interface\\Avionics-Lighting-Interface\\Log_Files\\";
         private string file_name = "";
 
         void SetFileName()
         {
-
+            file_name = $"Flight_{flight-info.flight_number}";
         }
         
         void WriteEvent(DateTime time, string id, string event_type)

2620459e7614737da66f0f078cf47aa8c9ed4b32
Holt570 <30250524@blackpool.ac.uk>
2025-11-02 08:04:57 +0000

    Created class and attributes for FlightInfo

---
+++ FlightInfo.cs
@@ -0,0 +1,14 @@
+
+
+namespace AircraftLightsGUI
+{
+    public class FlightInfo
+    {
+        string flight_number = "0";
+        DateTime takeoff_time;
+        DateTime landing_time;
+        DateTime sunset_time;
+        DateTime sunrise_time;
+        DateTime current_time;
+    }
+}
